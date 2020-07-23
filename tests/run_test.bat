447912407787809c898fd6c8908d90b246657b9b
Evgeny Mankov <evgeny.mankov@gmail.com>
2020-07-23 17:56:19 +0300

    [HIPIFY][#88] Compilation Database support via "-p" option and "compile_commands.json" file
    [feature]
    An ability for providing a Compilation Database in JSON format: https://clang.llvm.org/docs/JSONCompilationDatabase.html for the CUDA source to hipify.
    
    [usage]
    -p <folder containing compile_commands.json> or
    -p=<folder containing compile_commands.json>
    The compilation database file compile_commands.json should be provided or generated by clang based on cmake.
    
    [testing][windows]
    Test infrastructure for compile_commands.json generation + test on the "-p" option and compile_commands.json
    
    [ToDo]:
    - test infrastructure for Linux
    - update Readme.md

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -14,7 +14,20 @@ set all_args=%*
 call set clang_args=%%all_args:*%6=%%
 set clang_args=%6%clang_args%
 
-%HIPIFY% -o=%TMP_FILE% %IN_FILE% %CUDA_ROOT% %ROC% -- %clang_args%
+set test_dir=%~dp2
+set "test_dir=%test_dir:\=/%"
+
+set compile_commands=compile_commands.json
+set json_in=%test_dir%%compile_commands%.in
+set json_out=%test_dir%%compile_commands%
+
+if exist %json_in% (
+  powershell -Command "(gc %json_in%) -replace '<test dir>', '%test_dir%' -replace '<CUDA dir>', '%CUDA_ROOT%' | Out-File -encoding ASCII %json_out%"
+  %HIPIFY% -o=%TMP_FILE% %IN_FILE% %CUDA_ROOT% -p=%test_dir%
+) else (
+  %HIPIFY% -o=%TMP_FILE% %IN_FILE% %CUDA_ROOT% %ROC% -- %clang_args%
+)
+
 if errorlevel 1 (echo      Error: hipify-clang.exe failed with exit code: %errorlevel% && exit /b %errorlevel%)
 
 findstr /v /r /c:"[ ]*//[ ]*[CHECK*|RUN]" %TMP_FILE% | %FILE_CHECK% %IN_FILE%
