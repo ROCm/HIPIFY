8eb5d3b8c84a4e766969e1552f74f15c9edbe3f6
Evgeny Mankov <evgeny.mankov@gmail.com>
2020-12-14 16:15:17 +0300

    [HIPIFY][#84] Uppercase file extensions recognition

--- bin/findcode.sh
+++ bin/findcode.sh
@@ -2,7 +2,9 @@
 
 SEARCH_DIRS=$@
 
-find $SEARCH_DIRS -name '*.cu'
+find $SEARCH_DIRS -name '*.cu' -o -name '*.CU'
 find $SEARCH_DIRS -name '*.cpp' -o -name '*.cxx' -o -name '*.c' -o -name '*.cc'
-find $SEARCH_DIRS -name '*.cuh'
+find $SEARCH_DIRS -name '*.CPP' -o -name '*.CXX' -o -name '*.C' -o -name '*.CC'
+find $SEARCH_DIRS -name '*.cuh' -o -name '*.CUH'
 find $SEARCH_DIRS -name '*.h' -o -name '*.hpp' -o -name '*.inc' -o -name '*.inl' -o -name '*.hxx' -o -name '*.hdl'
+find $SEARCH_DIRS -name '*.H' -o -name '*.HPP' -o -name '*.INC' -o -name '*.INL' -o -name '*.HXX' -o -name '*.HDL'
