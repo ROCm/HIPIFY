b4081fc55ad8d8a72d816d8854495a1314c1aeca
Evgeny Mankov <evgeny.mankov@gmail.com>
2022-08-07 14:03:18 +0200

    [HIPIFY][BLAS] Satisfy CUDA 11.7.1 restriction
    [Restriction]
    + If both `cublas.h` and `cublas_v2.h` are included the following error occurs:
      `cublas.h(61,2): error: "It is an error to include both cublas.h and cublas_v2.h"`
    
    [Solution]
    + Swap `cublas_v2.h` and `cublas.h` includes if `cublas_v2.h` goes first
    + Actually, CUDA error message is misleading: erroneous to include `cublas_v2.h` before `cublas.h` only;
    + On the other hand, including them both is correct and only possible, when v1 and v2 APIs are needed to be used, cause
      not every API from `cublas.h` has a corresponding v2 analogue in `cublas_v2.h`

--- tests/unit_tests/headers/headers_test_07.cu
+++ tests/unit_tests/headers/headers_test_07.cu
@@ -1,8 +1,11 @@
 // RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
 
+// CHECK: #include <hip/hip_runtime.h>
 // CHECK: #include "hipblas.h"
 // CHECK-NOT: #include "cublas.h"
+// CHECK-NOT: #include "cublas_v2.h"
 // CHECK: #include <stdio.h>
-#include "cublas_v2.h"
 #include "cublas.h"
+#include "cublas_v2.h"
+// CHECK-NOT: #include "hipblas.h"
 #include <stdio.h>
