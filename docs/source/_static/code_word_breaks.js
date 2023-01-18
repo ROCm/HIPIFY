03e1e7d1a554ed4aca3d8dbd78267ded27d34f6a
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-01-18 13:31:49 -0700

    Use rocm-docs package for conf.py

--- docs/source/_static/code_word_breaks.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-    $('.table td code').each( function () {
-        text = $(this).text()
-        text = text.replaceAll(/_([^\u200B])/g, '_\u200B$1').replaceAll(/([a-z])([A-Z])/g, '$1\u200B$2')
-        $(this).text(text)
-    })
-})
