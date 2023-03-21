02bd6ad706e03633986a563f46f01f18fb9906e6
Sam Wu <samwu103@amd.com>
2023-03-21 10:24:20 -0600

    update mdlrc rules

--- docs/index.md
+++ docs/index.md
@@ -2,6 +2,6 @@
 
 hipify-clang and hipify-perl are tools to translate CUDA source code into portable HIP C++ automatically.
 
- * [hipify-clang](hipify-clang.md)
- * [hipify-perl](hipify-perl.md)
- * [Supported CUDA APIs](supported_apis.md)
+* [hipify-clang](hipify-clang.md)
+* [hipify-perl](hipify-perl.md)
+* [Supported CUDA APIs](supported_apis.md)
--- mdlrc-style.rb
+++ mdlrc-style.rb
@@ -1,7 +1,23 @@
+# Ref: https://github.com/DavidAnson/markdownlint/blob/main/doc/Rules.md
 all
 
-# Extend line length
-rule 'MD013', :line_length => 99999
-
+# Allow different Unordered list style
+exclude_rule 'MD004'
+# Allow different Unordered list indentation
+exclude_rule 'MD007'
+# Allow Multiple consecutive blank lines
+exclude_rule 'MD012'
+# Allow unlimited line length
+exclude_rule 'MD013'
+# Ignore Headers should be surrounded by blank lines
+exclude_rule 'MD022'
 # Allow Ordered list item prefix
 exclude_rule 'MD029'
+# Ignore Lists should be surrounded by blank lines
+exclude_rule 'MD032'
+# Allow Inline HTML
+exclude_rule 'MD033'
+# Allow Bare URLs
+exclude_rule 'MD034'
+# Ignore File should end with a single newline character
+exclude_rule 'MD047'
