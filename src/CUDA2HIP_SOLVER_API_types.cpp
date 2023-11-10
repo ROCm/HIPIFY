9e01a6a8c0725a216e75fc95c0db7df8fd4dd1e8
Evgeny Mankov <evgeniy.mankov@amd.com>
2023-11-10 21:27:32 +0100

    [HIPIFY][#675][#677][SOLVER][feature] cuSOLVER support - Step 4 - Data types
    + `cusolverStatus_t` partial support
    + [fix] `rocblas_status_invalid_pointer` -> `rocblas_status_invalid_value`, `rocblas_status_size_query_mismatch` -> `rocblas_status_arch_mismatch`
    + Updated the regenerated hipify-perl, affected synthetic tests, and CUDA2HIP documentation
    + [ToDo] `SOLVER` `CUDA2HIP` documentation generation

--- src/CUDA2HIP_SOLVER_API_types.cpp
+++ src/CUDA2HIP_SOLVER_API_types.cpp
@@ -24,8 +24,20 @@ THE SOFTWARE.
 
 // Map of all functions
 const std::map<llvm::StringRef, hipCounter> CUDA_SOLVER_TYPE_NAME_MAP {
-  {"cusolverStatus_t",                                      {"hipsolverStatus_t",                                    "rocblas_status",                                                CONV_LIB_FUNC, API_SOLVER, 1, HIP_EXPERIMENTAL}},
-  {"cusolverDnHandle_t",                                    {"hipsolverHandle_t",                                    "rocblas_handle",                                                CONV_LIB_FUNC, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"cusolverStatus_t",                                      {"hipsolverStatus_t",                                    "rocblas_status",                                                CONV_TYPE, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_SUCCESS",                               {"HIPSOLVER_STATUS_SUCCESS",                             "rocblas_status_success",                                        CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_NOT_INITIALIZED",                       {"HIPSOLVER_STATUS_NOT_INITIALIZED",                     "rocblas_status_invalid_handle",                                 CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_ALLOC_FAILED",                          {"HIPSOLVER_STATUS_ALLOC_FAILED",                        "rocblas_status_memory_error",                                   CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_INVALID_VALUE",                         {"HIPSOLVER_STATUS_INVALID_VALUE",                       "rocblas_status_invalid_value",                                  CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_ARCH_MISMATCH",                         {"HIPSOLVER_STATUS_ARCH_MISMATCH",                       "rocblas_status_arch_mismatch",                                  CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_MAPPING_ERROR",                         {"HIPSOLVER_STATUS_MAPPING_ERROR",                       "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_EXECUTION_FAILED",                      {"HIPSOLVER_STATUS_EXECUTION_FAILED",                    "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_INTERNAL_ERROR",                        {"HIPSOLVER_STATUS_INTERNAL_ERROR",                      "rocblas_status_internal_error",                                 CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED",             {"HIPSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED",           "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, UNSUPPORTED}},
+  {"CUSOLVER_STATUS_NOT_SUPPORTED",                         {"HIPSOLVER_STATUS_NOT_SUPPORTED",                       "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_ZERO_PIVOT",                            {"HIPSOLVER_STATUS_ZERO_PIVOT",                          "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, HIP_EXPERIMENTAL}},
+  {"CUSOLVER_STATUS_INVALID_LICENSE",                       {"HIPSOLVER_STATUS_INVALID_LICENSE",                     "rocblas_status_not_implemented",                                CONV_NUMERIC_LITERAL, API_SOLVER, 1, UNSUPPORTED}},
+  {"cusolverDnHandle_t",                                    {"hipsolverHandle_t",                                    "rocblas_handle",                                                CONV_TYPE, API_SOLVER, 1, HIP_EXPERIMENTAL}},
 };
 
 const std::map<llvm::StringRef, cudaAPIversions> CUDA_SOLVER_TYPE_NAME_VER_MAP {
@@ -33,5 +45,23 @@ const std::map<llvm::StringRef, cudaAPIversions> CUDA_SOLVER_TYPE_NAME_VER_MAP {
 
 const std::map<llvm::StringRef, hipAPIversions> HIP_SOLVER_TYPE_NAME_VER_MAP {
   {"hipsolverStatus_t",                                     {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_SUCCESS",                              {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_NOT_INITIALIZED",                      {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_ALLOC_FAILED",                         {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_INVALID_VALUE",                        {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_ARCH_MISMATCH",                        {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_MAPPING_ERROR",                        {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_EXECUTION_FAILED",                     {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_INTERNAL_ERROR",                       {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"HIPSOLVER_STATUS_NOT_SUPPORTED",                        {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
   {"hipsolverHandle_t",                                     {HIP_4050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status",                                        {HIP_3000, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_success",                                {HIP_3000, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_invalid_handle",                         {HIP_5060, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_memory_error",                           {HIP_5060, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_invalid_value",                          {HIP_3050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_not_implemented",                        {HIP_1050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_internal_error",                         {HIP_1050, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_status_arch_mismatch",                          {HIP_5070, HIP_0,    HIP_0,  HIP_LATEST}},
+  {"rocblas_handle",                                        {HIP_1050, HIP_0,    HIP_0,  HIP_LATEST}},
 };
--- tests/unit_tests/synthetic/libraries/cusolver2rocsolver.cu
+++ tests/unit_tests/synthetic/libraries/cusolver2rocsolver.cu
@@ -13,7 +13,27 @@ int main() {
   cusolverDnHandle_t handle;
 
   // CHECK: rocblas_status status;
+  // CHECK-NEXT: rocblas_status STATUS_SUCCESS = rocblas_status_success;
+  // CHECK-NEXT: rocblas_status STATUS_NOT_INITIALIZED = rocblas_status_invalid_handle;
+  // CHECK-NEXT: rocblas_status STATUS_ALLOC_FAILED = rocblas_status_memory_error;
+  // CHECK-NEXT: rocblas_status STATUS_INVALID_VALUE = rocblas_status_invalid_value;
+  // CHECK-NEXT: rocblas_status STATUS_ARCH_MISMATCH = rocblas_status_arch_mismatch;
+  // CHECK-NEXT: rocblas_status STATUS_MAPPING_ERROR = rocblas_status_not_implemented;
+  // CHECK-NEXT: rocblas_status STATUS_EXECUTION_FAILED = rocblas_status_not_implemented;
+  // CHECK-NEXT: rocblas_status STATUS_INTERNAL_ERROR = rocblas_status_internal_error;
+  // CHECK-NEXT: rocblas_status STATUS_NOT_SUPPORTED = rocblas_status_not_implemented;
+  // CHECK-NEXT: rocblas_status STATUS_ZERO_PIVOT = rocblas_status_not_implemented;
   cusolverStatus_t status;
+  cusolverStatus_t STATUS_SUCCESS = CUSOLVER_STATUS_SUCCESS;
+  cusolverStatus_t STATUS_NOT_INITIALIZED = CUSOLVER_STATUS_NOT_INITIALIZED;
+  cusolverStatus_t STATUS_ALLOC_FAILED = CUSOLVER_STATUS_ALLOC_FAILED;
+  cusolverStatus_t STATUS_INVALID_VALUE = CUSOLVER_STATUS_INVALID_VALUE;
+  cusolverStatus_t STATUS_ARCH_MISMATCH = CUSOLVER_STATUS_ARCH_MISMATCH;
+  cusolverStatus_t STATUS_MAPPING_ERROR = CUSOLVER_STATUS_MAPPING_ERROR;
+  cusolverStatus_t STATUS_EXECUTION_FAILED = CUSOLVER_STATUS_EXECUTION_FAILED;
+  cusolverStatus_t STATUS_INTERNAL_ERROR = CUSOLVER_STATUS_INTERNAL_ERROR;
+  cusolverStatus_t STATUS_NOT_SUPPORTED = CUSOLVER_STATUS_NOT_SUPPORTED;
+  cusolverStatus_t STATUS_ZERO_PIVOT = CUSOLVER_STATUS_ZERO_PIVOT;
 
   // CUDA: cusolverStatus_t CUSOLVERAPI cusolverDnCreate(cusolverDnHandle_t *handle);
   // ROC: ROCBLAS_EXPORT rocblas_status rocblas_create_handle(rocblas_handle* handle);
