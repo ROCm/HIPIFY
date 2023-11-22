74d1603e3e92f4b073d80593341875ca40b1953c
Sam Wu <sam.wu2@amd.com>
2023-11-22 14:45:18 -0700

    Standardize documentation
    Relates to https://github.com/RadeonOpenCompute/rocm-docs-core/issues/330

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,17 @@
 
 from rocm_docs import ROCmDocs
 
+# for PDF output on Read the Docs
+project = "HIPIFY Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+
+external_toc_path = "./sphinx/_toc.yml"
+
 docs_core = ROCmDocs("HIPIFY Documentation")
 docs_core.setup()
+
+external_projects_current_project = "hipify"
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
