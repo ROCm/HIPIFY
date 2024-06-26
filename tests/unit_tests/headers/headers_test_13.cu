f2608a84786d3a15de236a698b154ca7fa26b2b1
yuwei <ys4680@nyu.edu>
2024-06-26 13:33:44 -0400

    Update test and perl script for math_constants.h conversion

--- tests/unit_tests/headers/headers_test_13.cu
+++ tests/unit_tests/headers/headers_test_13.cu
@@ -1,7 +1,8 @@
 // RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
 
 // CHECK: #include <hip/hip_math_constants.h>
-// CHECK-NOT: #include <cuda_runtime.h>
+// CHECK: #include <hip/hip_runtime.h>
+// CHECK-NOT: #include <cuda.h>
 // CHECK: #include <stdio.h>
 #include <cuda.h>
 #include <math_constants.h>
