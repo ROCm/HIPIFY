4b8ce28378a04694486c80761da31dbbb6c9b013
Evgeny Mankov <evgeny.mankov@gmail.com>
2024-09-10 20:16:30 +0100

    [HIPIFY][RAND][tests][fix] Fix synthetic `RAND` tests

--- tests/unit_tests/synthetic/libraries/curand2hiprand.cu
+++ tests/unit_tests/synthetic/libraries/curand2hiprand.cu
@@ -1,4 +1,4 @@
-// RUN: %run_test hipify "%s" "%t" %hipify_args 3 --amap --skip-excluded-preprocessor-conditional-blocks --experimental %clang_args -D__CUDA_API_VERSION_INTERNAL -ferror-limit=500
+// RUN: %run_test hipify "%s" "%t" %hipify_args 3 --amap --default-preprocessor --experimental %clang_args -D__CUDA_API_VERSION_INTERNAL -ferror-limit=500
 
 // CHECK: #include <hip/hip_runtime.h>
 #include <cuda_runtime.h>
