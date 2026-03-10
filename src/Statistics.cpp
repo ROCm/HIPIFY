e0f6b3350f88b8d9c10a7006ffccfcacb8c57a29
Evgeny Mankov <evgeny.mankov@gmail.com>
2026-03-10 22:27:59 +0100

    [HIPIFY][DNN] `cuDNN 9.20.0` is the latest supported version (#2424)
    + Updated the regenerated `hipify-perl`, `DNN` `CUDA2HIP`, and `building` documentation

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -603,6 +603,7 @@ std::string Statistics::getCudaVersion(const cudaVersions &ver) {
     case CUDNN_9150: return "9.15.0";
     case CUDNN_9170: return "9.17.0";
     case CUDNN_9180: return "9.18.0";
+    case CUDNN_9200: return "9.20.0";
     case CUTENSOR_1010: return "1.0.1.0";
     case CUTENSOR_1100: return "1.1.0.0";
     case CUTENSOR_1200: return "1.2.0.0";
