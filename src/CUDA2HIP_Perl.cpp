9deba658484749bfa89c6094e4398b6857d35bc9
Evgeny Mankov <evgeny.mankov@gmail.com>
2026-03-19 13:50:37 +0100

    [HIPIFY][#1776][optimization] Remove redundant loops in Perl hash generator function `generateSimpleSubstitutions` - Part 1
    [Optimization]
    + Unnecessary nested O(N x M) loops in `generateSimpleSubstitutions` have been removed.
    
    [Reason]
    + Since Perl hashes are unordered, grouping dictionary insertions by conversion type is redundant
    
    [Targets]
    + Optimize, simplify, and clean up the C++ generator logic
    + This is a preliminary step for the primary target:
      - Drastically minimizing the footprint, size, and complexity of the generated `hipify-perl` in the subsequent steps
    
    [Justification]
    + `perl -c hipify-perl`
    + Comparing old and new `hipify-perl` outputs

--- src/CUDA2HIP_Perl.cpp
+++ src/CUDA2HIP_Perl.cpp
@@ -416,30 +416,22 @@ namespace perl {
 
   void generateSimpleSubstitutions(ostream &out) {
     out << endl << sub << sSimpleMappings << " {" << endl;
-    for (int i = 0; i < NUM_CONV_TYPES; ++i) {
-      if (i != CONV_INCLUDE_CUDA_MAIN_H && i != CONV_INCLUDE_CUDA_MAIN_V2_H && i != CONV_INCLUDE) {
-        for (auto &ma : CUDA_RENAMES_MAP()) {
-          if (Statistics::isUnsupported(ma.second) || Statistics::isHipExperimental(ma.second)) continue;
-          if (i == ma.second.type) {
-            out << tab << "$mappings{\"" << ma.first.str() << "\"} = { rep => \"" << ma.second.hipName.str() << "\", type => \"" << counterNames[ma.second.type] << "\" };" << endl;
-          }
-        }
+    for (auto &ma : CUDA_RENAMES_MAP()) {
+      if (Statistics::isUnsupported(ma.second) || Statistics::isHipExperimental(ma.second)) continue;
+      if (ma.second.type != CONV_INCLUDE_CUDA_MAIN_H && ma.second.type != CONV_INCLUDE_CUDA_MAIN_V2_H && ma.second.type != CONV_INCLUDE) {
+        out << tab << "$mappings{\"" << ma.first.str() << "\"} = { rep => \"" << ma.second.hipName.str() << "\", type => \"" << counterNames[ma.second.type] << "\" };" << endl;
       }
     }
     out << "}" << endl;
     out << endl << sub << sSimpleIncludes << " {" << endl;
-    for (int i = 0; i < NUM_CONV_TYPES; ++i) {
-      if (i == CONV_INCLUDE_CUDA_MAIN_H || i == CONV_INCLUDE_CUDA_MAIN_V2_H || i == CONV_INCLUDE) {
-        for (auto &ma : CUDA_INCLUDE_MAP) {
-          if (Statistics::isUnsupported(ma.second) || Statistics::isHipExperimental(ma.second)) continue;
-          if (i == ma.second.type) {
-            string sCUDA = ma.first.str();
-            string sHIP = ma.second.hipName.str();
-            sCUDA = regex_replace(sCUDA, regex("/"), "\\/");
-            sHIP = regex_replace(sHIP, regex("/"), "\\/");
-            out << tab << "subst(\"" << sCUDA << "\", \"" << sHIP << "\", \"" << counterNames[ma.second.type] << "\");" << endl;
-          }
-        }
+    for (auto &ma : CUDA_INCLUDE_MAP) {
+      if (Statistics::isUnsupported(ma.second) || Statistics::isHipExperimental(ma.second)) continue;
+      if (ma.second.type == CONV_INCLUDE_CUDA_MAIN_H || ma.second.type == CONV_INCLUDE_CUDA_MAIN_V2_H || ma.second.type == CONV_INCLUDE) {
+        string sCUDA = ma.first.str();
+        string sHIP = ma.second.hipName.str();
+        sCUDA = regex_replace(sCUDA, regex("/"), "\\/");
+        sHIP = regex_replace(sHIP, regex("/"), "\\/");
+        out << tab << "subst(\"" << sCUDA << "\", \"" << sHIP << "\", \"" << counterNames[ma.second.type] << "\");" << endl;
       }
     }
     out << "}" << endl;
