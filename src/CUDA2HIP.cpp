f06b3163fa754660f05fba4d3ddb67dbe47808ce
Evgeny Mankov <evgeny.mankov@gmail.com>
2026-04-02 21:47:50 +0200

    [HIPIFY][CUDA 13.1][FFT] `CUDA 13.1.0` support - Step 4 - `cuFFT Device` - Part 1 - `Data Types`
    + Updated the regenerated `hipify-perl` and `FFT` `CUDA2HIP` docs accordingly

--- src/CUDA2HIP.cpp
+++ src/CUDA2HIP.cpp
@@ -80,6 +80,7 @@ const std::map <llvm::StringRef, hipCounter> CUDA_INCLUDE_MAP {
   {"cufft.h",                                               {"hipfft/hipfft.h",                                       "",                                                               CONV_INCLUDE_CUDA_MAIN_H,    API_FFT, 0}},
   {"cufftXt.h",                                             {"hipfft/hipfftXt.h",                                     "",                                                               CONV_INCLUDE,                API_FFT, 0}},
   {"cufftw.h",                                              {"hipfft/hipfftw.h",                                      "",                                                               CONV_INCLUDE,                API_FFT, 0}},
+  {"cufft_device.h",                                        {"hipfft/hipfft_device.h",                                "",                                                               CONV_INCLUDE,                API_FFT, 0, UNSUPPORTED}},
   // cuSPARSE includes
   {"cusparse.h",                                            {"hipsparse.h",                                           "rocsparse.h",                                                    CONV_INCLUDE_CUDA_MAIN_H,    API_SPARSE, 0}},
   {"cusparse_v2.h",                                         {"hipsparse.h",                                           "rocsparse.h",                                                    CONV_INCLUDE_CUDA_MAIN_V2_H, API_SPARSE, 0}},
