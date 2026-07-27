a27e6519c19ed8e5366c03453ad05dd390a57c72
Aravindan Cheruvally <90783369+arvindcheru@users.noreply.github.com>
2026-07-27 11:42:07 -0400

    [HIPIFY][fix] Quote find output safely in bin wrapper scripts (#2675)
    * [HIPIFY][fix] Quote find output safely in bin wrapper scripts
    * [HIPIFY][fix] Preserve wrapper script behavior in SEC-00330 fix
    * [HIPIFY][fix] Avoid shift when wrapper scripts have no args

--- bin/findcode.sh
+++ bin/findcode.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SEARCH_DIRS=$@
+# Outputs matched CUDA/HIP source and header files, NUL-delimited.
 
-find $SEARCH_DIRS -name '*.cu' -o -name '*.CU'
-find $SEARCH_DIRS -name '*.cpp' -o -name '*.cxx' -o -name '*.c' -o -name '*.cc'
-find $SEARCH_DIRS -name '*.CPP' -o -name '*.CXX' -o -name '*.C' -o -name '*.CC'
-find $SEARCH_DIRS -name '*.cuh' -o -name '*.CUH'
-find $SEARCH_DIRS -name '*.h' -o -name '*.hpp' -o -name '*.hh' -o -name '*.inc' -o -name '*.inl' -o -name '*.hxx' -o -name '*.hdl'
-find $SEARCH_DIRS -name '*.H' -o -name '*.HPP' -o -name '*.HH' -o -name '*.INC' -o -name '*.INL' -o -name '*.HXX' -o -name '*.HDL'
+if [ "$#" -eq 0 ] || { [ "$#" -eq 1 ] && [ -z "$1" ]; }; then
+  set -- .
+fi
+
+find "$@" \( -name '*.cu' -o -name '*.CU' \) -print0
+find "$@" \( -name '*.cpp' -o -name '*.cxx' -o -name '*.c' -o -name '*.cc' \) -print0
+find "$@" \( -name '*.CPP' -o -name '*.CXX' -o -name '*.C' -o -name '*.CC' \) -print0
+find "$@" \( -name '*.cuh' -o -name '*.CUH' \) -print0
+find "$@" \( -name '*.h' -o -name '*.hpp' -o -name '*.hh' -o -name '*.inc' -o -name '*.inl' -o -name '*.hxx' -o -name '*.hdl' \) -print0
+find "$@" \( -name '*.H' -o -name '*.HPP' -o -name '*.HH' -o -name '*.INC' -o -name '*.INL' -o -name '*.HXX' -o -name '*.HDL' \) -print0
