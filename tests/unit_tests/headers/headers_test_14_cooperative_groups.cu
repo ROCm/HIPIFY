88dcfdd9987d75ebaa75c2072943d138efa4d0a9
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-08-06 14:04:15 +0100

    AIRUNTIME_171 - use CHECK_NEXT after the first CHECK as it is a better validation

--- tests/unit_tests/headers/headers_test_14_cooperative_groups.cu
+++ tests/unit_tests/headers/headers_test_14_cooperative_groups.cu
@@ -1,8 +1,8 @@
 // RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
 
 // CHECK: #include <hip/hip_cooperative_groups.h>
-// CHECK: #include <hip/cooperative_groups/hip_reduce.h>
-// CHECK: #include <hip/cooperative_groups/hip_scan.h>
+// CHECK-NEXT: #include <hip/cooperative_groups/hip_reduce.h>
+// CHECK-NEXT: #include <hip/cooperative_groups/hip_scan.h>
 #include <cooperative_groups.h>
 #include <cooperative_groups/reduce.h>
 #include <cooperative_groups/scan.h>
